03371eb4a4bc8a8bc8db1d98b9a8d7c61e23ffd2
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2020-08-23 12:19:26 -0300

    mudanca de nome do arquivo e programacao para usar como modulo

---
+++ python_scripts/secao_de_poicare_plots.py
@@ -0,0 +1,103 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import matplotlib.pyplot as pyplot
+import numpy as np
+import os
+import pandas as pandas
+
+# %%
+# template Jonathas Kennedy 26/03/2020
+# código deve estar antes de criar figura pelo pyplot.figure(figsize=(10, 10))
+# razão de tamnhos de fontes e ticks estabelecida para figuresize=10x10
+
+pyplot.rcParams['xtick.labelsize'] = 30
+pyplot.rcParams['ytick.labelsize'] = 30
+pyplot.rcParams['xtick.major.size'] = 12
+pyplot.rcParams['xtick.minor.size'] = 6
+pyplot.rcParams['ytick.major.size'] = 12
+pyplot.rcParams['ytick.minor.size'] = 6
+pyplot.rcParams['ytick.minor.visible'] = True
+pyplot.rcParams['xtick.minor.visible'] = True
+
+pyplot.rcParams['axes.titlesize'] = 20
+pyplot.rcParams['axes.labelpad'] = 20
+pyplot.rcParams['axes.labelsize'] = 30
+
+pyplot.rcParams['legend.fontsize'] = 16
+
+
+# %%
+# pasta onde armazena os plots
+def cria_pasta_plots():
+
+    path = "plots\\"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+# %%
+# import data
+def ler_dados():
+    data_donnell = pandas.read_csv('poincare.txt', header=None).to_numpy()
+    return data_donnell
+
+
+# %%
+# funcao gera e imprime grafico
+def gera_plot(path, data, ler, shape, total_variaveis):
+    for variavel in range(1, int(total_variaveis + 1)):
+
+        nomde_grafico = 'Secao de poincare C' + str(variavel)
+        print(nomde_grafico)
+        print("total de pontos plotados = %d" % (ler))
+
+        # matplotlib plot
+        fig = pyplot.figure(figsize=(10, 10))
+
+        data_1__x = []
+        data_1__y = []
+
+        print("total de seções de poincare=" + str(shape[0]))
+        for j in range(shape[0] - ler, shape[0]):
+            data_1__y.append(data[j, 2 * variavel])
+            data_1__x.append(data[j, 2 * variavel - 1])
+
+        config_plot = dict(marker='o', s=15, c='red', zorder=100)
+        pyplot.scatter(data_1__x, data_1__y, **config_plot)
+
+        # saving file to load in another python file
+        # https://stackoverflow.com/questions/48912527/how-to-join-two-matplotlib-figures
+        np.savez(path + "poincare_C%d.npz" % (variavel),
+                 method='scatter',
+                 args=(data_1__x, data_1__y),
+                 kwargs=config_plot)
+
+        pyplot.xlim(
+            min(data_1__x) - 0.01 * abs(min(data_1__x)),
+            max(data_1__x) + 0.01 * abs(max(data_1__x)))
+        pyplot.ylim(
+            min(data_1__y) - 0.01 * abs(min(data_1__y)),
+            max(data_1__y) + 0.01 * abs(max(data_1__y)))
+        pyplot.ylabel(r'$\dot{W_{' + str(variavel) + '}}/h$')
+        pyplot.xlabel(r'$W_{' + str(variavel) + '}/h$')
+        pyplot.ticklabel_format(axis='both', style='sci', scilimits=(0, 0))
+        pyplot.savefig(path + nomde_grafico + '.png',
+                       dpi=300,
+                       bbox_inches='tight')
+        # pyplot.show()
+        pyplot.close(fig)
+
+
+# %%
+if __name__ == "__main__":
+    path = cria_pasta_plots()
+    data = ler_dados()
+    shape = data.shape
+    # numero de seções a serem lidas
+    ler = None
+    if (ler == None or ler > shape[0]):
+        ler = shape[0]
+    total_variaveis = (shape[1] - 1) / 2
+    gera_plot(path, data, ler, shape, total_variaveis)
