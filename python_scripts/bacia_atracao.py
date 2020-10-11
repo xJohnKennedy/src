6abd19f82aa840f78d3157a20fbc27920f0bee66
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2020-10-11 17:12:43 -0300

    implementacao das divisoes nos eixos x e y  serem quaiquer;
    localizacao da celula agora e definida pelo seu numero de forma automatica baseado nas divisoes em x e y
    adicionado colormap customizado
    metodo de salvar grafico em backup adicionado

--- python_scripts/bacia_atracao.py
+++ python_scripts/bacia_atracao.py
@@ -56,99 +56,76 @@ def gera_plot(path, data, total_linhas, correcao_frequencia):
     pasta_hash: str = hashName.nome_hash(os.getcwd() + "\\" + path)
 
     dx, dy = 0.5, 0.5
-    n_div = 21
+    n_div_x = 21
+    n_div_y = 21
     y, x = np.mgrid[slice(-5 - dy / 2, 5 + dy, dy),
                     slice(-5 - dx / 2, 5 + dx, dx)]
 
     #computando norma de z
     z = []
+    z0 = np.zeros((n_div_x, n_div_y))
 
-    for i in range(total_linhas):
-        norma = np.linalg.norm(np.array([data[i, 3], data[i, 4]]), 2)
+    for i in data:
+        norma = np.linalg.norm(np.array([i[3], i[4]]), 2)
         z.append(norma)
 
-    z0 = np.zeros((n_div, n_div))
-
-    for i in range(n_div):
-        for j in range(n_div):
-            z0[j, i] = z[i * n_div + j]
+        # define linha em x
+        x_l = int((i[0] - 1) / n_div_y)
+        y_l = int((i[0] - 1) % n_div_y)
+        z0[y_l, x_l] = norma
 
     from matplotlib.colors import BoundaryNorm
     from matplotlib.ticker import MaxNLocator
+    from matplotlib.colors import ListedColormap
 
-    levels = MaxNLocator(nbins=15).tick_values(min(z), max(z))
+    levels = MaxNLocator(nbins=15).tick_values(0, max(z))
 
-    cmap = pyplot.get_cmap('PiYG')
+    cmap = ListedColormap(
+        ["white", "blue", "yellow", "green", "red", "gray", "black"])
     norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
-    fig, (ax0) = pyplot.subplots(nrows=1)
+    # matplotlib plot
+    # figura deve ser definida como subplots e retornas os axes para posterior configuracao do tick format
+    fig, ax = pyplot.subplots(1, 1, figsize=(10, 10))
 
-    im = ax0.pcolormesh(x, y, z0, cmap=cmap, norm=norm)
-    fig.colorbar(im, ax=ax0)
-    #ax0.set_title('pcolormesh with levels')
-    pyplot.show()
+    nomde_grafico = pasta_hash
+    print(nomde_grafico)
 
-    for variavel in range(1, int(total_linhas + 1)):
-        nomde_grafico = pasta_hash + '_C%s' % (variavel)
-        print(nomde_grafico)
-        col = 4 * variavel
-        print("col = %d" % (col))
+    config_plot = dict(cmap=cmap, norm=norm)
+    arg_plot = (x, y, z0)
+    im = ax.pcolormesh(*arg_plot, **config_plot)
+    #fig.colorbar(im, ax=ax)
 
-        # matplotlib plot
-        # figura deve ser definida como subplots e retornas os axes para posterior configuracao do tick format
-        fig, ax = pyplot.subplots(1, 1, figsize=(10, 10))
+    # saving file to load in another python file
+    # https://stackoverflow.com/questions/48912527/how-to-join-two-matplotlib-figures
+    np.savez(path + nomde_grafico + '.npz',
+             method='pcolormesh',
+             args=arg_plot,
+             kwargs=config_plot)
 
-        Data_caminhos = [0, 1]
-        minData1X = 1e9
-        maxData1X = -1e9
-        minData1Y = 1e9
-        maxData1Y = -1e9
+    minData1X = x.min()
+    maxData1X = x.max()
+    minData1Y = y.min()
+    maxData1Y = y.max()
 
-        for i in Data_caminhos:
-            data_1__x = []
-            data_1__y = []
+    pyplot.xlim(minData1X - 0.01 * abs(minData1X),
+                maxData1X + 0.01 * abs(maxData1X))
+    pyplot.ylim(minData1Y - 0.01 * abs(minData1Y),
+                maxData1Y + 0.01 * abs(maxData1Y))
 
-            shape = data[i].shape
-            for j in range(0, shape[0]):
-                data_1__y.append(data[i][j, col])
-                data_1__x.append(data[i][j, 1] * correcao_frequencia)
-            print("Nummero de pontos caminho %i = %i" % (i + 1, shape[0]))
-
-            config_plot = dict(marker='o', color='blue', s=10)
-            pyplot.scatter(data_1__x, data_1__y, **config_plot)
-
-            # saving file to load in another python file
-            # https://stackoverflow.com/questions/48912527/how-to-join-two-matplotlib-figures
-            np.savez(path + nomde_grafico + '_c_' + str(i + 1) + '.npz',
-                     method='scatter',
-                     args=(data_1__x, data_1__y),
-                     kwargs=config_plot)
-
-            #calculo maximos e minimos do grafico
-            minData1X = min([min(data_1__x), minData1X])
-            maxData1X = max([max(data_1__x), maxData1X])
-            minData1Y = min([min(data_1__y), minData1Y])
-            maxData1Y = max([max(data_1__y), maxData1Y])
-
-        pyplot.xlim(minData1X - 0.01 * abs(minData1X),
-                    maxData1X + 0.01 * abs(maxData1X))
-        pyplot.ylim(minData1Y - 0.01 * abs(minData1Y),
-                    maxData1Y + 0.01 * abs(maxData1Y))
-        pyplot.ylabel(r'$W_{' + str(variavel) + '}/h$')
-        pyplot.xlabel(r'$\omega_{1}/\omega_{0}$')
-        pyplot.ticklabel_format(axis='both',
-                                style='sci',
-                                scilimits=(0, 0),
-                                useOffset=False)
-        #ax.xaxis.set_major_formatter(pyplot.FuncFormatter('{:.2f}'.format))
-        #ax.yaxis.set_major_formatter(pyplot.FuncFormatter('{:.2f}'.format))
-        pyplot.savefig(path + nomde_grafico + '.png',
-                       dpi=300,
-                       bbox_inches='tight')
-        #pyplot.show()
-        pyplot.cla()
-        pyplot.clf()
-        pyplot.close('all')
+    pyplot.ylabel(r'$\dot{W}_{' + str(1) + '}/h$')
+    pyplot.xlabel(r'$W_{' + str(1) + '}/h$')
+    pyplot.ticklabel_format(axis='both',
+                            style='sci',
+                            scilimits=(0, 0),
+                            useOffset=False)
+    #ax.xaxis.set_major_formatter(pyplot.FuncFormatter('{:.2f}'.format))
+    #ax.yaxis.set_major_formatter(pyplot.FuncFormatter('{:.2f}'.format))
+    pyplot.savefig(path + nomde_grafico + '.png', dpi=300, bbox_inches='tight')
+    #pyplot.show()
+    pyplot.cla()
+    pyplot.clf()
+    pyplot.close('all')
 
 
 # %%
