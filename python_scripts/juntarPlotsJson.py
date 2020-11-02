a293f7ec67cda9afecbef9c40bb3ba55608bb304
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2020-11-01 23:33:09 -0300

    adicionado metodo para juntar plots com configuracoes personalizadas apartir da leitura de arquivo JSON

--- python_scripts/juntarPlotsJson.py
+++ python_scripts/juntarPlotsJson.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as pyplot
 import numpy as np
 import os
-import pandas as pandas
+import json
 
 # %%
 ## template Jonathas Kennedy 26/03/2020
@@ -30,70 +30,92 @@ pyplot.rcParams['legend.fontsize'] = 16
 pyplot.rcParams['agg.path.chunksize'] = 10000
 
 
+#%%
+# ler arquivo JSON
+def lerJson(nomeDoArquivo: str):
+    try:
+        nomeDoArquivo = nomeDoArquivo + ".json"
+        arquivo = open(nomeDoArquivo, 'r')
+        dados = json.load(arquivo)
+        return dados
+    except:
+        os._exit()
+
+
 # %%
 #https://stackoverflow.com/questions/48912527/how-to-join-two-matplotlib-figures
-def join(ax, files):
-    data = [np.load(filename, allow_pickle=True) for filename in files]
-    for datum in data:
-        method = getattr(ax, datum['method'].item())
-        args = tuple(datum['args'])
-        kwargs = datum['kwargs'].item()
-        method(*args, **kwargs)
+def join(ax, filename: str, data_plots):
+    data = carrega_plot(filename)
+
+    method = data['method'].item()
+    method = getattr(ax, method)
+    args = tuple(data['args'])
+    kwargs = data['kwargs'].item()
+    #configuracao do plot de acordo com os dados passados pelo arquivo json
+    try:
+        #caso o nome do arquivo possua .npz
+        filename = filename.split(".")
+        config_plot = data_plots[filename[0]]
+        for i in config_plot:
+            kwargs.update({i: config_plot[i]})
+    except:
+        pass
+    method(*args, **kwargs)
+
+
+def carrega_plot(nome):
+    try:  #tenta carregar arquivo
+        data = np.load(nome, allow_pickle=True)
+    except:
+        try:  #tenta carregar arquivo com a adicao do sufixo .npz
+            data = np.load(nome + ".npz", allow_pickle=True)
+        except:
+            os._exit()
+
+    return data
 
 
 # %%
-variavel = 1
-nomde_grafico = 'bifurcacao C' + str(variavel)
-print(nomde_grafico)
+def gera_plot(files, data_plots):
 
-# matplotlib plot
-fig, ax = pyplot.subplots(figsize=(10, 10))
+    nomde_grafico = data_plots["nomeGrafico"]
+    print(nomde_grafico)
 
-files = [
-    'cbadee92a5c10baaffb58296b75cda0b_C1_c_1.npz',
-    'cbadee92a5c10baaffb58296b75cda0b_C1_c_2.npz',
-    '14f5d2e0ab6ac702250b31999825c353_C1_c_1.npz',
-    '14f5d2e0ab6ac702250b31999825c353_C1_c_2.npz'
-]
+    # matplotlib plot
+    fig, ax = pyplot.subplots(figsize=(10, 10))
 
-#join(ax, files)
+    # funcao para juntar os plots
+    for nomeArquivo in files:
+        join(ax, nomeArquivo, data_plots)
 
-data = np.load(files[0], allow_pickle=True)
-method = getattr(ax, data['method'].item())
-args = tuple(data['args'])
-kwargs = data['kwargs'].item()
-method(*args, **kwargs)
+    # plota linha horizontal
+    comandos = data_plots["LinhaVertical"]
+    if (comandos["plotar"] == True):
+        kwargs = comandos["kwargs"]
+        ax.axvline(x=comandos["local_x"], **kwargs)
 
-data = np.load(files[1], allow_pickle=True)
-method = getattr(ax, data['method'].item())
-args = tuple(data['args'])
-kwargs = data['kwargs'].item()
-method(*args, **kwargs)
+    pyplot.xlim(0.8, 1.2)
+    """pyplot.ylim(
+        min(data_1__y) - 0.1 * abs(min(data_1__y)),
+        max(data_1__y) + 0.1 * abs(max(data_1__y))) """
+    pyplot.ylabel(data_plots["nomeEixoY"])
+    pyplot.xlabel(data_plots["nomeEixoX"])
+    #pyplot.ticklabel_format(axis='both', style='sci', scilimits=(0, 0))
+    pyplot.savefig("" + nomde_grafico + '.png', dpi=300, bbox_inches='tight')
+    pyplot.savefig("" + nomde_grafico + '.svg', dpi=300, bbox_inches='tight')
+    #pyplot.show()
+    pyplot.cla()
+    pyplot.clf()
+    pyplot.close('all')
 
-data = np.load(files[2], allow_pickle=True)
-method = getattr(ax, data['method'].item())
-args = tuple(data['args'])
-kwargs = data['kwargs'].item()
-kwargs.update({'color': 'gray'})
-method(*args, **kwargs)
 
-data = np.load(files[3], allow_pickle=True)
-method = getattr(ax, data['method'].item())
-args = tuple(data['args'])
-kwargs = data['kwargs'].item()
-kwargs.update({'color': 'gray'})
-method(*args, **kwargs)
+def main_func(ler=None):
+    user_input = str(input("\nNome do arquivo (JSON):  "))
+    arquivo_json = lerJson(user_input)
+    nome_arquivos = arquivo_json["nomePlots"]
+    gera_plot(nome_arquivos, arquivo_json)
 
-pyplot.xlim(0.8, 1.2)
-"""pyplot.ylim(
-    min(data_1__y) - 0.1 * abs(min(data_1__y)),
-    max(data_1__y) + 0.1 * abs(max(data_1__y))) """
-pyplot.ylabel(r'$W_{' + str(variavel + 7) + ',1}/h$')
-pyplot.xlabel(r'$\omega_{1}/\omega_{0}$')
-#pyplot.ticklabel_format(axis='both', style='sci', scilimits=(0, 0))
-pyplot.savefig("" + nomde_grafico + '.png', dpi=300, bbox_inches='tight')
-pyplot.savefig("" + nomde_grafico + '.svg', dpi=300, bbox_inches='tight')
-#pyplot.show()
-pyplot.cla()
-pyplot.clf()
-pyplot.close('all')
+
+# %%
+if __name__ == "__main__":
+    main_func()
