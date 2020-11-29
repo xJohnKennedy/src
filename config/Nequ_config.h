64d45db63ed52b78316f4c1b5dbd0185f6a2eb08
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2020-11-29 17:20:06 -0300

    corrigido definicao de variavel global n_max_thread pois esta e uma variavel determinada pelo user
    documentacao melhorada da variavel Nt_temp para evitar confusao com o numero de divisoes do RK

--- config/Nequ_config.h
+++ config/Nequ_config.h
@@ -2,10 +2,8 @@
 #define Nequ 42 //  Numero de equacoes do sistema
 
 #ifdef _FORCA_BR_C
+// nao confundir esta variavel com o numero de divisoes do RK,
+//esta variavel determina o numero de periodos que o forca_bruta ira executar antes de testar a convergencia novamente
 int		Nt_temp = 500;
 #endif // _FORCA_BR_C
-
-#ifdef _BACIA_C
-int const n_max_thread = 4;
-#endif // _BACIA_C
 
