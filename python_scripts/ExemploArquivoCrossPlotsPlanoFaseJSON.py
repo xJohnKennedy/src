88c4780ec47e3389cb4f59e2b3f180d20af0cf3f
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2021-01-18 17:12:25 -0300

    adicionado modelo de arquivo json modelo

---
+++ python_scripts/ExemploArquivoCrossPlotsPlanoFaseJSON.py
@@ -0,0 +1,12 @@
+### O arquivo Json não aceita comentarios
+### apagar os comentarios bloco de comentario e renomear a extensao do arquivo para .json
+""" 
+===>>> inicio do bloco de comentario
+
+### SYNTAXE DO ARQUIVOS JSON
+{
+    "crossPlots": array com as combinações a serem lidas para o crossPlot, ex:[C1xdC1dt, C1xdC3dt] 
+}
+===>>> termino do bloco de comentario
+"""
+{"crossPlots": [C1xdC1dt, C1xdC3dt]}
