cb4d5335df92f118be03cc36b79e1e30a5f98709
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2021-01-31 15:59:59 -0300

    rebasing branch 'feature/py_plots/impGeradorNomesBifurcacaoRessonancia' on 'd2ec21b'
    adicionado arquivo
    traducao da string de entrada colocada como modulo
    adicionado traducao de string para a ressonancia.py
    adicionado traducao de string para a bifurcacao.py

--- python_scripts/espectroCargaFrequencia.py
+++ python_scripts/espectroCargaFrequencia.py
@@ -36,7 +36,7 @@ pyplot.rcParams['agg.path.chunksize'] = 10000
 def cria_pasta_plots(pontos):
 
     path = "espectro_freq_carga"
-    path = path + "_c%s" % (pontos)
+    path = path + pontos
     path = path + "\\"
     if not os.path.exists(path):
         os.makedirs(path)
@@ -48,7 +48,7 @@ def cria_pasta_plots(pontos):
 def ler_dados(pontos):
     data = []
     for i in pontos:
-        print("..\\ponto %s\\force.txt\n" % (i))
+        print("..\\ponto %s\\force.txt" % (i))
         data.append(
             pandas.read_csv("..\\ponto %s\\force.txt" % (i),
                             header=None).to_numpy())
@@ -171,33 +171,10 @@ def gera_plot(path, data_plot_freq, data_plot_duracao_carga,
 
 # %%
 if __name__ == "__main__":
+    import stringParserPoints
     pontos = str(input("Caminhos para plotar <caminho 1,caminho 2>: "))
-    path = cria_pasta_plots(pontos)
-    if '$' in pontos:
-
-        tradutor = pontos.split("-")
-        nome = tradutor[0]
-        #conta ocorrencias de $ na string formadora dos nomes
-        count = 0
-        for i in nome:
-            if i == '$':
-                count = count + 1
-        conjunto_gerador = [nome]
-        conjunto_gerado = []
-        for i in range(count):
-            #obtem os codigos a substituir
-            cod_subs = tradutor[i + 1]
-            if '#' in cod_subs:
-                cod_subs = cod_subs.split("#")
-                cod_subs = range(int(cod_subs[0]), int(cod_subs[1]) + 1)
-            for j in conjunto_gerador:
-                for k in cod_subs:
-                    conjunto_gerado.append(j.replace('$', str(k), 1))
-            conjunto_gerador = conjunto_gerado
-            conjunto_gerado = []
-        pontos = conjunto_gerador
-    else:
-        pontos = pontos.split(",")
+    pontos, palavraPath = stringParserPoints.SeparaString(pontos)
+    path = cria_pasta_plots(palavraPath)
     data = ler_dados(pontos)
     data = filtraTrechos(data)
     data_plot_freq, data_plot_duracao_carga, data_plot_carga_inicial = geraDadosBarras(
