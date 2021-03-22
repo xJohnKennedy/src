635a3e85ecd6040ddf6687bc9864f021b68ac053
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2021-03-22 03:13:18 -0300

    teste

--- config/Kutta_header_config.h
+++
@@ -1,6 +0,0 @@
-#include "Nequ_config.h"
-
-double Pi = 3.14159265358979323846; //3.14159265359;
-double Div = 100.0;
-int Ndiv = 250; //  Numero de divisoes para integracao do sistema
-/***********************************************************/
